581fe1b7c850ebeedb4c9f0b356cfc7b1d754144
Varun Khatri <43516016+khatrivarun@users.noreply.github.com>
2020-10-09 10:58:39 +0530

    Initial commit

---
+++ user/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    template_name = "user/register.html"
+    context = {"form": form}
+
+    if form.is_valid():
+        form.save()
+        redirect('/')
+    else:
+        form = UserCreationForm()
+
+    return render(request, template_name, context)
