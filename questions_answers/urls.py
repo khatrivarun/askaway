288137869e0a3f9f0232981331c2d2622c0bbabd
Varun Khatri <43516016+khatrivarun@users.noreply.github.com>
2020-10-27 13:07:47 +0530

    Completed 🎉

--- questions_answers/urls.py
+++ questions_answers/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('question/<str:slug>/', list_single_question),
     path('question/<str:slug>/noice/<int:answer_id>/', vote),
     path('question/<str:slug>/update/', update_single_question),
+    path('question/<str:slug>/delete/', delete_question),
     path('question/<str:slug>/answer/', create_answer),
     path('question/<str:slug>/answer/<int:answer_id>/delete/', delete_answer),
     path('create/', create_question)
